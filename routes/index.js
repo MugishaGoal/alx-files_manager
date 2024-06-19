a7982f12183a416745a2825db00ef8c275e011da
Mugisha Goal <degaullemugisha@gmail.com>
2024-06-19 12:39:11 -0700

    removed unexpected file extention in import

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import AppController from '../controllers/AppController.js';
+import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
